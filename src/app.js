472522f5c57afbb7b6ba2164f6e2ffbc9bfc7853
raj pandey <raj.pandey@contentstack.com>
2023-02-09 15:58:27 +0530

    fetching data from url and changed margin of the cards in the cards container

--- src/app.js
+++ src/app.js
@@ -1,56 +1,49 @@
 import Header from "./HeaderComponent.js";
 import CardContainer from "./CardContainer.js";
 import Cards from "./Cards.js";
-import data from '../data/data.json' assert { type: 'json' };
+import data from "../data/data.json" assert { type: "json" };
 import Cart from "./Cart.js";
 import CartCards from "./CartCards.js";
 
 class AppComponent {
-    constructor () {
-        this.header = new Header();
-        this.cardContainer = new CardContainer();
-        this.cart = new Cart();
-        this.root = document.getElementById('root');
-        this.mainComponent = document.createElement('main');
-    }
-    render () {
-        const cardContainerRender = this.cardContainer.render();
-        const cartRender = this.cart.render();
+  constructor() {
+    this.header = new Header();
+    this.cardContainer = new CardContainer();
+    this.cart = new Cart();
+    this.root = document.getElementById("root");
+    this.mainComponent = document.createElement("main");
+  }
+  render() {
+    const cardContainerRender = this.cardContainer.render();
+    const cartRender = this.cart.render();
 
-        this.root.append(this.header.render());
-        this.root.appendChild(this.mainComponent);
-        this.mainComponent.append(cardContainerRender);
-        this.mainComponent.append(cartRender);
+    this.root.append(this.header.render());
+    this.root.appendChild(this.mainComponent);
+    this.mainComponent.append(cardContainerRender);
+    this.mainComponent.append(cartRender);
 
-
-        data.products.forEach((element)=>{
-        const card = new Cards(element);
-        cardContainerRender.appendChild(card.render());
+    const url = "https://dummyjson.com/products";
+    fetch(url)
+      .then((response) => response.json())
+      .then((res2) => {
+        res2.products.forEach((element) => {
+          const card = new Cards(element);
+          cardContainerRender.appendChild(card.render());
         });
-    }
-    mount () {}
+      });
+  }
+  mount() {}
 }
 
 const ac = new AppComponent();
 ac.render();
-
-
-
-
-
-
 
 // //Create instances of classes
 // const header = new Header();
 // const cardContainer = new CardContainer();
 // const cart = new Cart();
 
-
-
-
 // console.log(data);
-
-
 
 // data.products.forEach((element)=>{
 //     const cartCards = new CartCards(element);
